1dbcbd821a119cd2e681053017eecdeeda4e9528
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-13 08:11:04 +0100

    debug

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title><?php $title    ?></title>
+    <title><?php $title="";    ?></title>
 </head>
 <body>
     
